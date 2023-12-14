a296530630799319f82234d60e6865aaf318acaa
ASt39 <alexandr.statut@gmail.com>
2023-12-14 19:49:28 +0200

    Created todo module and todo entity.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './modules/todo/todo.module';
+import { Todo } from './modules/todo/entities/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'qwerty',
+      database: 'myfirst',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
